50e013587347b4c9c7555cc5af2d2387b1186182
Wolvan <wolvan1@gmail.com>
2015-09-06 06:44:52 +0200

    Create infrastructure files

---
+++ newxkitbot.conf.js
@@ -0,0 +1,13 @@
+module.exports = {
+	GitterBridge: {
+		ENABLED: true,
+		
+		Gitter_Channel: "",
+		Gitter_Username: "",
+		Gitter_Password: "",
+		
+		IRC_Channel: "",
+		IRC_Username: "",
+		IRC_Password: "",
+	},
+}
