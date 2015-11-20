924c134940c7269dc3d2434f5b59a0c0fc44dbe8
Wolvan <wolvan1@gmail.com>
2015-11-20 13:15:38 +0100

    Apply license properly
    Put the license information in each source file the way GPL-2.0 says
    and remove the "How to apply this license" part from LICENSE.md

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+/*
+	A multi-functional bot for the NXKit Team
+    Copyright (C) 2015 Wolvan
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+console.log("NewXKitBot version 2, Copyright (C) 2015 Wolvan\nNewXKitBot comes with ABSOLUTELY NO WARRANTY\n\n");
 console.log("Starting up NewXKitBot v2.0 by Wolvan...\n");
 
 var GitterIRCBridge = require("./modules/GitterIRCBridge.js");
